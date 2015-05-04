efff877a464f053b51f0ade6ba7b6d6a2d4797e0
vague315 <vague315@gmail.com>
2015-05-04 00:09:01 -0500

    Updated to test new functionality

--- src/game/SidescrollerGame.java
+++ src/game/SidescrollerGame.java
@@ -3,11 +3,14 @@ package game;
 import static org.lwjgl.glfw.GLFW.*;
 import game.core.VariableTimestepGame;
 import game.entity.Block;
+import game.entity.Player;
 import game.input.KeyInput;
 import game.input.MouseInput;
 import graphics.opengl.Texture;
 
 import java.awt.Color;
+
+import math.Vector2f;
 
 import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
@@ -21,7 +24,9 @@ public class SidescrollerGame extends VariableTimestepGame{
 	private GLFWCursorPosCallback mousePosCallback;
 	private Texture texture;
 	
-	private Block block;
+	private Block floor;
+	private Player player;
+	private Player player2;
 	
 	public SidescrollerGame() {
 		super();
@@ -42,30 +47,25 @@ public class SidescrollerGame extends VariableTimestepGame{
 
 	@Override
 	public void initGameObjects() {
-		block = new Block(150, 200, 25, 25);
+		player = new Player(250, 400, 55, 115);
+		player2 = new Player(1250, 400, 55, 115);
+		floor = new Block(1000, 50, 2000, 50);
+		//renderer.getCamera().followEntity(player);
+		//renderer.getCamera().followEntity(floor);
 	}
 
 	@Override
 	public void updateGameObjects(float delta) {
-		
+		player.update(delta);
 	}
 
 	@Override
 	public void renderGameObjects(float alpha) {		
 		texture.bind(); // binds a simple texture to be used for drawing
-		renderer.setActiveLayer(0);
-//		renderer.drawRect(200, 100, 45, 45, Color.white);
-		renderer.drawCircle(600, 500, 50, Color.red);
-		renderer.drawCircleOutline(400, 500, 50, Color.red);
-//		renderer.setActiveLayer(1);
-//		renderer.drawLine(0, 0, 800, 400, Color.red);
-		renderer.setActiveLayer(2);
-		renderer.drawRectOutline(500, 300, 150, 75, Color.cyan);
-		renderer.setActiveLayer(1);
-		renderer.drawRect(500, 100, 15, 75, new Color(255,162,0));
-		renderer.setActiveLayer(2);
-		block.render(renderer, alpha);
-//		renderer.drawRect(500, 75, 50, 50, Color.red);
+		renderer.setActiveLayer(1);		
+		floor.debugRender(renderer, alpha);
+		player.debugRender(renderer, alpha);
+		player2.debugRender(renderer, alpha);		
 	}
 
 	@Override
@@ -78,17 +78,30 @@ public class SidescrollerGame extends VariableTimestepGame{
 
 	@Override
 	public void input() {
+		Vector2f playerVel = new Vector2f();
 		if(KeyInput.isKeyDown(GLFW_KEY_D)){
-			renderer.moveCamera(5, 0, 0);
+			playerVel = playerVel.add(new Vector2f(5.0f, 0.0f));
 		}
 		if(KeyInput.isKeyDown(GLFW_KEY_A)){
-			renderer.moveCamera(-5, 0, 0);
+			playerVel = playerVel.add(new Vector2f(-5.0f, 0.0f));
 		}
 		if(KeyInput.isKeyDown(GLFW_KEY_W)){
-			renderer.moveCamera(0, 5, 0);
+			playerVel = playerVel.add(new Vector2f(0.0f, 5.0f));
 		}
 		if(KeyInput.isKeyDown(GLFW_KEY_S)){
-			renderer.moveCamera(0, -5, 0);
+			playerVel = playerVel.add(new Vector2f(0.0f, -5.0f));
+		}
+		if(KeyInput.isKeyDown(GLFW_KEY_1)){
+			renderer.getCamera().panCameraTo(player, 5);
+		}
+		if(KeyInput.isKeyDown(GLFW_KEY_2)){
+			renderer.getCamera().panCameraTo(player2, 5);
+		}
+		if(KeyInput.isKeyDown(GLFW_KEY_3)){
+			renderer.getCamera().followEntity(player);
+		}
+		if(KeyInput.isKeyDown(GLFW_KEY_4)){
+			renderer.getCamera().followEntity(player2);
 		}
 		if(KeyInput.isKeyDown(GLFW_KEY_ESCAPE)){
 			long id = glfwGetCurrentContext();
@@ -100,6 +113,8 @@ public class SidescrollerGame extends VariableTimestepGame{
 		if(MouseInput.isButtonPressed(GLFW_MOUSE_BUTTON_2)){
 			
 		}
+		
+		player.setVelocity(playerVel);
 	}
 
 	@Override
@@ -117,13 +132,16 @@ public class SidescrollerGame extends VariableTimestepGame{
 		else if(ups<30)
 			uc = Color.red;
 		
-		float [] pos = MouseInput.getMousePosition();
+		float [] mousePos = MouseInput.getMousePosition();
+		float [] camPos = {	renderer.getCamera().getFocus().x, renderer.getCamera().getFocus().y };
 			
 		renderer.setActiveLayer(3);
 		renderer.disableCamera();
 		renderer.drawDebugText("FPS:"+timer.getFPS(), 950, 745, fc);
 		renderer.drawDebugText("UPS:"+timer.getUPS(), 950, 725, uc);
-		renderer.drawDebugText("Mouse: " + pos[0] + ", " + pos[1], 10, 745, Color.white);
+		renderer.drawDebugText("Mouse: " + mousePos[0] + ", " + mousePos[1], 10, 745, Color.white);
+		renderer.drawDebugText("Player: " + player.getPosition().x + ", " + player.getPosition().x, 10, 725, Color.white);
+		renderer.drawDebugText("Camera: " + camPos[0] + ", " + camPos[1], 10, 705, Color.white);
 		renderer.enableCamera();
 	}
 
